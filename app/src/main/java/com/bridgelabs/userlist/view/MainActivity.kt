ff1dfb0ed16960903da54dab14b6b22041f5d268
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-23 14:34:41 +0530

    [soundarya]Add. added MainActivity.json into view directory

---
+++ app/src/main/java/com/bridgelabs/userlist/view/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.bridgelabs.userlist.view
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.bridgelabs.userlist.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    // Array of strings...
+    private val simpleList: ListView by lazy { findViewById<ListView>(R.id.simpleListView) }
+    var countryList =
+        arrayOf("India", "China", "australia", "Portugle", "America", "NewZealand")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var arrayAdapter = ArrayAdapter<String>(
+            this,
+            R.layout.activity_listview,
+            R.id.textView, countryList
+        )
+        simpleList.adapter = arrayAdapter
+    }
+}
