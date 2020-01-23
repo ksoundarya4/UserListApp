31a079af7c07cf10a8380bb108868b2b25797055
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-23 13:05:54 +0530

    [soundarya]Add. User.kt

---
+++ app/src/main/java/com/bridgelabs/userlist/model/User.kt
@@ -0,0 +1,12 @@
+/**
+ * User List App
+ * @description User data class having name
+ * and mobile number.
+ * @file User.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 23/01/2020
+ */
+package com.bridgelabs.userlist.model
+
+data class User(val name : String, val mobileNumber : String)
