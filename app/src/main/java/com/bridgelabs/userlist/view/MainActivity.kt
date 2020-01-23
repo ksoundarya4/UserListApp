3ed94d1086821d39d2c7cf7068722382e9445c44
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-23 18:43:38 +0530

    [soundarya ]Refactor. build.gradle

--- app/src/main/java/com/bridgelabs/userlist/view/MainActivity.kt
+++ app/src/main/java/com/bridgelabs/userlist/view/MainActivity.kt
@@ -5,22 +5,31 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.bridgelabs.userlist.R
+import com.bridgelabs.userlist.model.User
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
 
     // Array of strings...
     private val simpleList: ListView by lazy { findViewById<ListView>(R.id.simpleListView) }
-    var countryList =
-        arrayOf("India", "China", "australia", "Portugle", "America", "NewZealand")
+    var userName = ArrayList<String>()
+    var mobileNumber = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var arrayAdapter = ArrayAdapter<String>(
+
+        val objectMapper = jacksonObjectMapper()
+        val userList: ArrayList<User> =
+            objectMapper.readValue(File("/home/user/AndroidStudioProjects/UserList/app/src/main/java/com/bridgelabs/userlist/model/user.json"))
+
+        val arrayAdapter = ArrayAdapter<User>(
             this,
             R.layout.activity_listview,
-            R.id.textView, countryList
+            R.id.name, userList
         )
         simpleList.adapter = arrayAdapter
     }
