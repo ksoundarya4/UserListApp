2b23618f21e79ed4ddd7969d488bfa41ec24e24f
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-24 15:34:45 +0530

    [soundarya]Refactor. DisplayActivity.kt

---
+++ app/src/main/java/com/bridgelabs/userlist/view/DisplayActivity.kt
@@ -0,0 +1,13 @@
+package com.bridgelabs.userlist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bridgelabs.userlist.R
+
+class DisplayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display)
+    }
+}
