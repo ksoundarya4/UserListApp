829f3230dae50d69237c120e17928e8c211aa88d
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-24 19:20:41 +0530

    [soundarya]Refactor. AndroidMainifest.xml

--- app/src/main/java/com/bridgelabs/userlist/view/DisplayActivity.kt
+++ app/src/main/java/com/bridgelabs/userlist/view/DisplayActivity.kt
@@ -1,10 +1,11 @@
 package com.bridgelabs.userlist.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.widget.EditText
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.bridgelabs.userlist.R
 import com.bridgelabs.userlist.model.User
 
@@ -21,5 +22,13 @@ class DisplayActivity : AppCompatActivity() {
         val editUserNumber = findViewById<TextView>(R.id.user_number)
         val numberHolder = intent.getSerializableExtra("UserInfo") as User
         editUserNumber.setText(numberHolder.mobileNumber)
+
+        val deleteButton  = findViewById<Button>(R.id.delete_user)
+
+        deleteButton.setOnClickListener(View.OnClickListener {
+            val main = MainActivity()
+            val user: ListView = main.findViewById(R.id.simpleListView)
+            user
+        })
     }
 }
