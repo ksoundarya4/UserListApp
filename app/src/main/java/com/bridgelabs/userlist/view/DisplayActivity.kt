588f447496e68589e338fb4ca504aac9ca9a076b
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-24 17:51:57 +0530

    [soundarya]Refactor. DisplayActivity.kt

--- app/src/main/java/com/bridgelabs/userlist/view/DisplayActivity.kt
+++ app/src/main/java/com/bridgelabs/userlist/view/DisplayActivity.kt
@@ -2,12 +2,24 @@ package com.bridgelabs.userlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.EditText
+import android.widget.TextView
 import com.bridgelabs.userlist.R
+import com.bridgelabs.userlist.model.User
 
 class DisplayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
+
+        val editUserName = findViewById<TextView>(R.id.user_name)
+        val nameHolder = intent.getSerializableExtra("UserInfo") as User
+        editUserName.setText(nameHolder.name)
+
+        val editUserNumber = findViewById<TextView>(R.id.user_number)
+        val numberHolder = intent.getSerializableExtra("UserInfo") as User
+        editUserNumber.setText(numberHolder.mobileNumber)
     }
 }
