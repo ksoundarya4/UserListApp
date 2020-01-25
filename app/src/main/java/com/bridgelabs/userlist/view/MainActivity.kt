92ff5aa1c568f47171477697a334b36e2e5119f6
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-25 18:09:18 +0530

    [soundarya]Refactor. MainActivity.kt

--- app/src/main/java/com/bridgelabs/userlist/view/MainActivity.kt
+++ app/src/main/java/com/bridgelabs/userlist/view/MainActivity.kt
@@ -1,34 +1,28 @@
 package com.bridgelabs.userlist.view
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bridgelabs.userlist.R
-import com.bridgelabs.userlist.R.id.user_name
+import com.bridgelabs.userlist.model.FileSystem
 import com.bridgelabs.userlist.model.User
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import kotlinx.android.synthetic.main.activity_listview.*
 import java.io.File
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View {
 
     // Array of strings...
-    val simpleList: ListView by lazy { findViewById<ListView>(R.id.simpleListView) }
+    private val simpleList: ListView by lazy { findViewById<ListView>(R.id.simpleListView) }
 
-    /**To read array of user json file from device*/
-    private val objectMapper = jacksonObjectMapper()
-    val file = File("/data/data/com.bridgelabs.userlist/files/user.json")
+    private val PICK_USER_REQUEST = 1
 
-    val userList: Array<User> = objectMapper.readValue(file)
-
-    /**To obtain the position of list view */
-    var listPosition: Int = 0
-
-    @SuppressLint("SdCardPath")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,8 +31,14 @@ class MainActivity : AppCompatActivity() {
         val addButton = findViewById<Button>(R.id.add_user)
         addButton.setOnClickListener {
             val addActivityIntent = Intent(this, AddActivity::class.java)
-            startActivity(addActivityIntent)
+            startActivityForResult(addActivityIntent, PICK_USER_REQUEST)
         }
+
+
+
+        /**To read list of user*/
+        val fileSystem = FileSystem()
+        val userList: ArrayList<User> = fileSystem.readUser(assets.open("user.json"))
 
         /**To get user names into array list of userNames */
         val userNames = ArrayList<String>()
@@ -50,18 +50,19 @@ class MainActivity : AppCompatActivity() {
         val arrayAdapter = ArrayAdapter<String>(
             this,
             R.layout.activity_listview,
-            user_name, userNames
+            R.id.user_name, userNames
         )
         simpleList.adapter = arrayAdapter
 
         simpleList.onItemClickListener =
-            AdapterView.OnItemClickListener { parent, view, position, id ->
+            AdapterView.OnItemClickListener { _, _, position, _ ->
 
                 // Getting userList click value into String variable.
                 // Getting userList click value into String variable.
                 val userListViewClickedValue: User = userList[position]
                 Log.d("User", userListViewClickedValue.toString())
-                listPosition = position
+
+
                 Log.d("Position", position.toString())
 
                 val displayActivityIntent = Intent(this, DisplayActivity::class.java).apply {
