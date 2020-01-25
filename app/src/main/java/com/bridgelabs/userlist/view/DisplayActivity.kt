4770d40c28627f7ee22c7fe293c437e7ee20ca49
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-25 18:08:58 +0530

    [soundaryaRefactor DisplayActivity.kt

--- app/src/main/java/com/bridgelabs/userlist/view/DisplayActivity.kt
+++ app/src/main/java/com/bridgelabs/userlist/view/DisplayActivity.kt
@@ -13,32 +13,29 @@ package com.bridgelabs.userlist.view
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bridgelabs.userlist.R
 import com.bridgelabs.userlist.model.User
 
-class DisplayActivity : AppCompatActivity() {
+class DisplayActivity : AppCompatActivity(),com.bridgelabs.userlist.view.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
-        val editUserName = findViewById<TextView>(R.id.user_name)
+        val editUserName = findViewById<TextView>(R.id.display_user_name)
         val nameHolder = intent.getSerializableExtra("UserInfo") as User
         editUserName.setText(nameHolder.name)
 
-        val editUserNumber = findViewById<TextView>(R.id.user_number)
+        val editUserNumber = findViewById<TextView>(R.id.display_user_number)
         val numberHolder = intent.getSerializableExtra("UserInfo") as User
         editUserNumber.setText(numberHolder.mobileNumber)
 
         val deleteButton = findViewById<Button>(R.id.delete_user)
 
         deleteButton.setOnClickListener(View.OnClickListener {
-            val main = MainActivity()
-            val user = main.simpleList
-            user.removeViewAt(main.listPosition)
+
         })
     }
 }
