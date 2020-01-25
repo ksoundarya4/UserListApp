e52d5b4636e088d7c1c69f9483f7fd371b9ba472
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-25 18:07:28 +0530

    [soundarya]Add. AddActivity.kt,ListActivity.kt

---
+++ app/src/main/java/com/bridgelabs/userlist/list_module/view/AddActivity.kt
@@ -0,0 +1,13 @@
+package com.bridgelabs.userlist.list_module.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bridgelabs.userlist.R
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add2)
+    }
+}
---
+++ app/src/main/java/com/bridgelabs/userlist/list_module/view/ListActivity.kt
@@ -0,0 +1,44 @@
+package com.bridgelabs.userlist.list_module.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.bridgelabs.userlist.R
+import com.bridgelabs.userlist.list_module.list_contract.ListPresenterContract
+import com.bridgelabs.userlist.list_module.list_contract.ListViewContract
+import com.bridgelabs.userlist.list_module.model.ListModelContractImpl
+import com.bridgelabs.userlist.list_module.presenter.ListPresenterImpl
+import com.bridgelabs.userlist.model.User
+
+class ListActivity : AppCompatActivity(), ListViewContract {
+
+    private val usersListView: ListView by lazy { findViewById<ListView>(R.id.listview_users) }
+    private val presenterContract: ListPresenterContract by lazy {
+        ListPresenterImpl(
+            this,
+            ListModelContractImpl()
+        )
+    }
+    private val PICK_USER_REQUEST = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+
+        presenterContract.initUI()
+    }
+
+    override fun initListView(arrayList: List<User>) {
+        val arrayAdapter =
+            ArrayAdapter<User>(this, R.layout.activity_listview, R.id.user_name, arrayList)
+        usersListView.adapter = arrayAdapter
+    }
+
+    override fun navigateToAddActivity() {
+        val addActivityIntent = Intent(this, AddActivity::class.java)
+
+        startActivityForResult(addActivityIntent, PICK_USER_REQUEST)
+    }
+}
