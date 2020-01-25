d77b3a5a5777105b83c06ba151edcc83281c0e91
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-25 10:59:08 +0530

    [soundarya]Refactor. Added addButton instance to mainActivity.kt

--- app/src/main/java/com/bridgelabs/userlist/view/MainActivity.kt
+++ app/src/main/java/com/bridgelabs/userlist/view/MainActivity.kt
@@ -4,10 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bridgelabs.userlist.R
 import com.bridgelabs.userlist.R.id.user_name
@@ -20,20 +17,28 @@ import java.io.File
 class MainActivity : AppCompatActivity() {
 
     // Array of strings...
-    private val simpleList: ListView by lazy { findViewById<ListView>(R.id.simpleListView) }
+    val simpleList: ListView by lazy { findViewById<ListView>(R.id.simpleListView) }
 
     /**To read array of user json file from device*/
-  private val objectMapper = jacksonObjectMapper()
-    private val userList: Array<User> =
-        objectMapper.readValue(File("/data/data/com.bridgelabs.userlist/files/user.json"))
+    private val objectMapper = jacksonObjectMapper()
+    val file = File("/data/data/com.bridgelabs.userlist/files/user.json")
+
+    val userList: Array<User> = objectMapper.readValue(file)
 
     /**To obtain the position of list view */
-    var listPosition : Int = 0
+    var listPosition: Int = 0
 
     @SuppressLint("SdCardPath")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**To add User to userList*/
+        val addButton = findViewById<Button>(R.id.add_user)
+        addButton.setOnClickListener {
+            val addActivityIntent = Intent(this, AddActivity::class.java)
+            startActivity(addActivityIntent)
+        }
 
         /**To get user names into array list of userNames */
         val userNames = ArrayList<String>()
@@ -57,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                 val userListViewClickedValue: User = userList[position]
                 Log.d("User", userListViewClickedValue.toString())
                 listPosition = position
-                Log.d("Position",position.toString())
+                Log.d("Position", position.toString())
 
                 val displayActivityIntent = Intent(this, DisplayActivity::class.java).apply {
                     putExtra(
