78040ebbeb6107f8fcb590556c91ea5f26f912aa
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-25 10:52:55 +0530

    [soundarya]Add. AddActivity.kt

---
+++ app/src/main/java/com/bridgelabs/userlist/view/AddActivity.kt
@@ -0,0 +1,36 @@
+/**
+ * UserListApp
+ * @purpose To get user name and user mobile number input
+ * from user and add them to user list on clicking save button.
+ * @file AddActivity.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 25/01/2020
+ */
+package com.bridgelabs.userlist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.bridgelabs.userlist.R
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_activivty)
+
+        val editUserName = findViewById<EditText>(R.id.add_user_name)
+        val userName = editUserName.text.toString()
+
+        val editUserNumber = findViewById<EditText>(R.id.add_user_mobile_number)
+        val userNumber = editUserNumber.text.toString()
+
+        val saveButton = findViewById<Button>(R.id.save_user)
+        saveButton.setOnClickListener {
+            val main = MainActivity()
+
+        }
+    }
+}
