c974eaafd0e109e7e4712f52effe5fb3bee0dd99
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-25 12:51:50 +0530

    [soundarya]Add. Model.kt interface

---
+++ app/src/main/java/com/bridgelabs/userlist/model/Model.kt
@@ -0,0 +1,14 @@
+/**
+ * UserListAPP
+ * @purpose TO create a file system class which
+ * can write to .json file and read from .json file
+ * @file Model.kt
+ * @author ksoundarya4
+ * @version 1.0
+ * @since 25/01/2020
+ */
+package com.bridgelabs.userlist.model
+
+/**Model interface*/
+interface Model {
+}
