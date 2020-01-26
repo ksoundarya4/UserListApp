e6b4fa3d832b578b1d7d4b2838b9e9b7c44a5593
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-26 09:31:00 +0530

    [soundarya]Refactor. changed User import package name in ListPresenter.kt

--- app/src/main/java/com/bridgelabs/userlist/list_module/presenter/ListPresenterImpl.kt
+++ app/src/main/java/com/bridgelabs/userlist/list_module/presenter/ListPresenterImpl.kt
@@ -3,7 +3,6 @@ package com.bridgelabs.userlist.list_module.presenter
 import com.bridgelabs.userlist.list_module.list_contract.ListModelContract
 import com.bridgelabs.userlist.list_module.list_contract.ListPresenterContract
 import com.bridgelabs.userlist.list_module.list_contract.ListViewContract
-import com.bridgelabs.userlist.model.User
 
 class ListPresenterImpl(val view: ListViewContract, val model: ListModelContract) : ListPresenterContract {
 
