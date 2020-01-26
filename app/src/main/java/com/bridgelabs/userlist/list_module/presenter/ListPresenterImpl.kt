5dd6fb489a5789f3ee79dd29bf9dcbf4a13009f7
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-26 18:32:32 +0530

    [soundarya]Refactor. overriden onItemClick ListPresenterImpl.kt

--- app/src/main/java/com/bridgelabs/userlist/list_module/presenter/ListPresenterImpl.kt
+++ app/src/main/java/com/bridgelabs/userlist/list_module/presenter/ListPresenterImpl.kt
@@ -3,8 +3,10 @@ package com.bridgelabs.userlist.list_module.presenter
 import com.bridgelabs.userlist.list_module.list_contract.ListModelContract
 import com.bridgelabs.userlist.list_module.list_contract.ListPresenterContract
 import com.bridgelabs.userlist.list_module.list_contract.ListViewContract
+import com.bridgelabs.userlist.list_module.model.User
 
-class ListPresenterImpl(val view: ListViewContract, val model: ListModelContract) : ListPresenterContract {
+class ListPresenterImpl(val view: ListViewContract, val model: ListModelContract) :
+    ListPresenterContract {
 
     override fun initUI() {
         val userList = model.getListOfUsers()
@@ -13,5 +15,11 @@ class ListPresenterImpl(val view: ListViewContract, val model: ListModelContract
 
     override fun onAddButtonClick() {
         view.navigateToAddActivity()
+    }
+
+    override fun onItemClick(position : Int) {
+        val userList = model.getListOfUsers()
+        val clickedUser : User = userList[position]
+        view.navigateToDetailActivity(clickedUser)
     }
 }
