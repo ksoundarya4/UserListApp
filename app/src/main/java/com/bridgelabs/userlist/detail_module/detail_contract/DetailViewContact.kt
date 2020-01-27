c1996bdfe5ff7d35cd0c3495e92aa2a1ae8b2fce
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-27 10:26:19 +0530

    [soundarya]Refactor. removed unused imports

--- app/src/main/java/com/bridgelabs/userlist/detail_module/detail_contract/DetailViewContact.kt
+++ app/src/main/java/com/bridgelabs/userlist/detail_module/detail_contract/DetailViewContact.kt
@@ -1,6 +1,7 @@
 package com.bridgelabs.userlist.detail_module.detail_contract
 
 import android.view.View
+import com.bridgelabs.userlist.util.User
 import java.io.InputStream
 
 interface DetailViewContact {
