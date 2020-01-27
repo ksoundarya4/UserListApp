d42922b3a5ee7604d67799d5a6ef35d2438f83e6
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-27 10:25:12 +0530

    [soundarya]Refactor. added getListOfUsers to DetailModelContract.kt

--- app/src/main/java/com/bridgelabs/userlist/detail_module/detail_contract/DetailModelContract.kt
+++ app/src/main/java/com/bridgelabs/userlist/detail_module/detail_contract/DetailModelContract.kt
@@ -2,4 +2,7 @@ package com.bridgelabs.userlist.detail_module.detail_contract
 
 import com.bridgelabs.userlist.util.User
 
-interface DetailModelContract
+interface DetailModelContract{
+    fun deleteUser(user : User)
+    fun getListOfUsers() : List<User>
+}
