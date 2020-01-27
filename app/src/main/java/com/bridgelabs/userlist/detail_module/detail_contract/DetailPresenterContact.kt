56ac5416a9ee31cd7f0ddeebd3a237cefaef3e61
ksoundarya4 <ksoundarya4@gmail.com>
2020-01-27 10:25:47 +0530

    [soundarya]Refactor. added getUserList to DetailPresenterContract.kt

--- app/src/main/java/com/bridgelabs/userlist/detail_module/detail_contract/DetailPresenterContact.kt
+++ app/src/main/java/com/bridgelabs/userlist/detail_module/detail_contract/DetailPresenterContact.kt
@@ -1,8 +1,10 @@
 package com.bridgelabs.userlist.detail_module.detail_contract
 
-import android.view.View
+import com.bridgelabs.userlist.util.User
+import java.io.InputStream
 
 interface DetailPresenterContact {
     fun initDetailUI()
     fun onDeleteButtonClick(userName : String , userNumber : String)
+    fun getUserList(inputStream: InputStream) : List<User>
 }
